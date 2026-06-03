10b2c5d2fa38e98f3c4ebc8572ddbc5a2adc6970
IchikaCoding <IchikaCoding@users.noreply.github.com>
2026-06-03 22:18:37 +0900

    プロジェクトファイルを追加。

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// 改行あり
+// Consoleはクラス、WriteLine()はクラスのメソッド？
+// ""の中身はLiteral文字列
+Console.WriteLine("This is the first line.");
+Console.WriteLine("This is the second line.");
+// 改行なし
+Console.Write("Hello");
+Console.Write("Ichika");
