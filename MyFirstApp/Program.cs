0aee23cbe06e0bde20426cc168d8f2a0507ad294
IchikaCoding <IchikaCoding@users.noreply.github.com>
2026-06-04 21:32:36 +0900

    fix: 昨日の復習

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -1,9 +1,59 @@
 ﻿// 改行あり
 // Consoleはクラス、WriteLine()はクラスのメソッド？
 // ""の中身はLiteral文字列
-Console.WriteLine("This is the first line.");
+
 Console.WriteLine("This is the second line.");
+
 // 改行なし
 Console.Write("Hello");
 Console.Write("Ichika");
-Console.Write("♡");
+Console.Write("｡°(°¯᷄◠¯᷅°)°｡");
+
+
+// 演習
+
+Console.WriteLine("b");
+Console.WriteLine(123);
+// floatリテラルを作成するためにF追加
+Console.WriteLine(0.25f); // 0.25
+// doubleLiteral
+Console.WriteLine(3.141592); // 3.141592
+// decimal 型
+Console.WriteLine(3.141592m); // 3.141592
+
+// boolLiteral
+Console.WriteLine(true); // True
+Console.WriteLine(false); // Flase
+
+
+
+// Output: 123
+Console.WriteLine("123");
+// Output: 123
+Console.WriteLine(123);
+
+// Output: true
+Console.WriteLine("true");
+// Output: True
+Console.WriteLine(true);
+
+// 値を代入しているから型推論が働くのでvarでOK
+var firstName = "ichika";
+
+// 変数名にデータ型をいれると良くないらしいけどこれはどうなの？
+string stringName = "name";
+
+
+
+char userOption;
+
+int gameScore;
+// decimalっぽさある？
+decimal particlesPerMillion;
+// これってboolっぽさある？
+bool processedCustomer;
+
+
+// =============2026/06/04=================
+
+
